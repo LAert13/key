8a0159caa9d03c3af82b9a7b8bc30b1a3c98b91a
Denis Petrenko <petrenko.den@gmail.com>
2014-04-14 09:02:19 +0300

    update left nav

--- include/leftNav.php
+++ include/leftNav.php
@@ -16,18 +16,16 @@ $categories = formatCategories($categories, $catId);
             <ul>
                 <li><a href="<?php echo $_SERVER['PHP_SELF']; ?>">Все товары</a></li>
 
-                <?php foreach ($categories as $category) {
+                <?php
+                $isFirst = true;
+                $categoryOpened = false;
+                foreach ($categories as $category) {
                     extract($category);
                     // now we have $cat_id, $cat_parent_id, $cat_name
 
                     $level = ($cat_parent_id == 0) ? 1 : 2;
                     $url   = $_SERVER['PHP_SELF'] . "?c=$cat_id";
 
-                    // for second level categories we print extra spaces to give
-                    // indentation look
-                    if ($level == 2) {
-                        $cat_name = '&nbsp; &nbsp; &raquo;&nbsp;' . $cat_name;
-                    }
 
                     // assign id="current" for the currently selected category
                     // this will highlight the category name
@@ -35,9 +33,26 @@ $categories = formatCategories($categories, $catId);
                     if ($cat_id == $catId) {
                         $listId = ' id="current"';
                     }
+
+                    // for second level categories we print extra spaces to give
+                    // indentation look
+                    if ($level == 1) {
+                        echo $isFirst ? '' : $categoryOpened ? '</ul></li>' : '</li>';
+                        $categoryOpened = false;
+                    ?>
+                    <li<?php echo $listId; ?>><a href="<?php echo $url; ?>"><?php echo $cat_name; ?></a>
+                    <?php } else {
+                        if ($categoryOpened == false) { $categoryOpened = true; ?><ul><?php }?>
+                        <li<?php echo $listId; ?>><a href="<?php echo $url; ?>"><?php echo $cat_name; ?></a></li>
+                    <?php }
+
+
+
+                    $isFirst = false;
+                }
+
+                echo $categoryOpened ? '</ul></li>' : '</li>';
                 ?>
-                <li<?php echo $listId; ?>><a href="<?php echo $url; ?>"><?php echo $cat_name; ?></a></li>
-                <?php } ?>
             </ul>
         </div>
 
